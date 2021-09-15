ab40ba374e9a119921c394c8e422a0efa8775c08
Maricarmen M <17030815@itcelaya.edu.mx>
2021-09-15 00:50:16 -0500

    mi primer commit

---
+++ lib/src/screens/intensiones_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class IntensionesScreen extends StatefulWidget {
+  const IntensionesScreen({ Key? key }) : super(key: key);
+
+  @override
+  _IntensionesScreenState createState() => _IntensionesScreenState();
+}
+
+class _IntensionesScreenState extends State<IntensionesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        
+      ],
+      
+    );
+  }
+}
